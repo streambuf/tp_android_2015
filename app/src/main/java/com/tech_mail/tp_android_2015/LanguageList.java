bf28ce12202ae0e0adba12f4f64590ac2febb071
streambuf <streambuf@mail.ru>
2015-02-15 18:55:20 +0300

    HttpResponseGetter

--- app/src/main/java/com/tech_mail/tp_android_2015/LanguageList.java
+++ app/src/main/java/com/tech_mail/tp_android_2015/LanguageList.java
@@ -1,6 +1,5 @@
 package com.tech_mail.tp_android_2015;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,44 +7,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
+import com.tech_mail.tp_android_2015.utils.HttpResponseGetter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class LanguageList extends ActionBarActivity {
-
 
     // TODO
     private final String URL = "https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20150213T145944Z.ca111d9a559d26b2.d078d31f6d32d5c17e70ba9ffeca68ea26b0269d";
+
     private final String API_ARRAY_NAME = "dirs";
-    private String streamToString(InputStream is) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
-        for (String line = r.readLine(); line != null; line =r.readLine()){
-            sb.append(line);
-        }
-        is.close();
-        return sb.toString();
-    }
 
     private List getListFromJSON (JSONArray jArray) throws JSONException {
         List <String> languageList = new ArrayList<>();
@@ -56,12 +30,10 @@ public class LanguageList extends ActionBarActivity {
         return languageList;
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_list);
-
 
         String default_langs = "";
         new DownloadLanguageList(default_langs)
@@ -79,17 +51,16 @@ public class LanguageList extends ActionBarActivity {
         }
 
         protected String doInBackground(String... urls) {
-            String urldisplay = urls[0];
             String downloads = null;
             try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                JSONObject json = new JSONObject(streamToString(in));
+                JSONObject json = HttpResponseGetter.getResponseByUrl(urls[0]);
                 downloads = getListFromJSON(json.getJSONArray(API_ARRAY_NAME)).toString();
             } catch (Exception e) {
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
             }
             return downloads;
+
         }
 
         protected void onPostExecute(String result) {
